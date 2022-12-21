b374a941f54c811ace7b25ab367ebefd4de9678c
parthivn25 <121167284+parthivn25@users.noreply.github.com>
2022-12-21 03:45:21 -0800

    Add files via upload

--- SinglePendulum.py
+++ SinglePendulum.py
@@ -16,37 +16,36 @@ g = 9.81
 def derivative(y, t, l, m):
     theta, omega = y
     # insert derivative as defined by Euler-Lagrange
-    dydt = [omega, (g / l) * math.sin(theta)]
+    dydt = [omega, - (g / l) * math.sin(theta)]
     return dydt
 
 
-# give initial conditions for pendulum
-theta_naught, omega_naught = 90, 0.001
+# give initial conditions for pendulum, theta given in degrees
+theta_naught, omega_naught = 60, 0
 
 # create an array of times for the process, followed by initial conditions
-intervals = 1001
+intervals = 100
 t = math.linspace(0, 10, intervals)
-y_naught = math.array([theta_naught, omega_naught])
+y_naught = math.array([theta_naught*math.pi/180, omega_naught])
 
 # create vector array y with theta and omega for at any time
 solution = odeint(derivative, y_naught, t, args=(l, m))
 x = []
 y = []
+
 # change to cartesian coordinates
 for i in range(intervals):
     x.append(l * math.sin(solution[i][0]))
-    y.append(l * math.cos(solution[i][0]))
+    y.append(-l * math.cos(solution[i][0]))
 
 ax = plt.figure().add_subplot(projection='3d')
-
 plt.plot(x, y, t)
-plt.xlabel('x label')
-plt.ylabel('y label')
-
-plt.title("test plot")
-
-
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Time')
+plt.xticks(range(-2, 3))
+plt.yticks(range(-2, 3))
+plt.title("Pendulum Position as a Parametric Equation of Time")
 plt.show(block=True)
 plt.interactive(False)
-
 plt.show
