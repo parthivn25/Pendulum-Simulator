f0830fcd618a00e7c8e27f4a73ae30fc065aa3fe
parthivn25 <121167284+parthivn25@users.noreply.github.com>
2022-12-22 12:16:16 -0800

    Add files via upload

--- SinglePendulum.py
+++ SinglePendulum.py
@@ -2,7 +2,7 @@ import numpy as math
 import imageio
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
-
+import os
 
 # work in standard units of meters, seconds, and radians
 
@@ -23,11 +23,12 @@ def derivative(y, t, l, m):
 
 
 # give initial conditions for pendulum, theta given in degrees
-theta_naught, omega_naught = 60, 0
+theta_naught, omega_naught = 179, 10
 
 # create an array of times for the process, followed by initial conditions
-intervals = 50
-t = math.linspace(0, 5, intervals)
+intervals = 100
+seconds = 10
+t = math.linspace(0, seconds, intervals)
 y_naught = math.array([theta_naught * math.pi / 180, omega_naught])
 
 # create vector array y with theta and omega for at any time
@@ -40,25 +41,33 @@ for i in range(intervals):
     x.append(l * math.sin(solution[i][0]))
     y.append(-l * math.cos(solution[i][0]))
 
+
 # function creates frames at each portion of time
 def create_frame(r):
+    # plot the pendulum and the bar connecting to the origin
     plt.plot(x[r], y[r], marker="o", markersize=7, markeredgecolor="red", markerfacecolor="black")
-    plt.xlim([-2, 2])
-    plt.xlabel('x')
-    plt.ylim([-2, 2])
-    plt.ylabel('y')
-    plt.title(f'Pendulum position at {r}')
+    plt.plot([x[r], 0], [y[r], 0], 'k-')
+    # add axis preferences and titles to graph
+    plt.xlim([-1.5, 1.5])
+    plt.ylim([-1.5, 1.5])
+    plt.xticks([])
+    plt.yticks([])
+    seconddisplay = (r / intervals) * seconds
+    seconddisplay= round(seconddisplay, 2)
+    plt.title(f'Pendulum position at {seconddisplay} seconds')
     plt.savefig(f'img_{r}.png')
     plt.close()
 
-#calls function for frames
+
+# calls function for frames, creating at different times throughout motion
 for r in range(intervals):
     create_frame(r)
 frames = []
-#appends all frames into an array and uses imageio to make it into a gif
+# appends all frames into an array and uses imageio to make it into a gif
 for r in range(intervals):
     image = imageio.v2.imread(f'img_{r}.png')
     frames.append(image)
-#resultant gif is saved
+# resultant gif is saved
+
 imageio.mimsave('./example.gif', frames, fps=15)
 
